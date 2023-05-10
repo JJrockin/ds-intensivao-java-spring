14be6ffe1a9b13d4068a2474206ce4b7c30980f6
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-05-10 11:20:29 -0300

    Adding images and edit Controller

--- src/main/java/com/jjrockin/dslist/controllers/GameController.java
+++ src/main/java/com/jjrockin/dslist/controllers/GameController.java
@@ -3,6 +3,7 @@ package com.jjrockin.dslist.controllers;
 import com.jjrockin.dslist.dto.GameMinDTO;
 import com.jjrockin.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
-
+    @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
         return result;
