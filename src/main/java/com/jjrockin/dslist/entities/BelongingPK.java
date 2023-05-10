6f42db74eafb7e251eda7cc3689ff18e104dd54f
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-05-10 15:18:06 -0300

    Domain model

---
+++ src/main/java/com/jjrockin/dslist/entities/BelongingPK.java
@@ -0,0 +1,37 @@
+package com.jjrockin.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
---
+++ src/main/java/com/jjrockin/dslist/entities/GameList.java
@@ -0,0 +1,34 @@
+package com.jjrockin.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
